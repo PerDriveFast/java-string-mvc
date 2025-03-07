b9053ebf8ea01a960a1fccb9306c0e54f17eaadb
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-03-07 20:16:54 +0700

    component

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -1,13 +1,23 @@
 package vn.hoidanit.laptopshop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		//SpringApplication.run(LaptopshopApplication.class, args);
+
+		// container
+		ApplicationContext hoidanIt = SpringApplication.run(LaptopshopApplication.class, args);
+for(String s: hoidanIt.getBeanDefinitionNames())
+{
+	System.out.println(s);
+}
+
 	}
 
 }
