cece6cb26a338d5c79d770a6f8402c464795f7a8
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-03-07 20:40:49 +0700

    string boot devtool

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
 	public String index() {
-		return "Hello World from Spring Boot Trung @!";
+		return "Hello World  Trung @!";
 	}
-
+	 @GetMapping("/user")
+	public String UserPage() {
+		return "Only users can asscess this page.";
+	}
+	 @GetMapping("/admin")
+	public String AdminPage() {
+		return "Only admin can asscess this page.";
+	}
 }
