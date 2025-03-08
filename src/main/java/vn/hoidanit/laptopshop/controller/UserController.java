71baad11570b7d38c264d70be6ac329b2ba38e83
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-03-08 21:08:46 +0700

    jstl in view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -6,6 +6,7 @@ import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,8 +23,10 @@ public class UserController {
 
     @RequestMapping("/")
 
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("trung", test);
+        model.addAttribute("trungdanit", "from controller with model");
         return "hello";
     }
 }
--- src/main/webApp/WEB-INF/view/hello.jsp
+++ src/main/webApp/WEB-INF/view/hello.jsp
@@ -1,10 +1,20 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 <body>
     Hello from jsp
+    <h1>
+        ${trung}
+    </h1>
+    <h2>
+        ${trungdanit}
+    </h2>
 </body>
 </html>
