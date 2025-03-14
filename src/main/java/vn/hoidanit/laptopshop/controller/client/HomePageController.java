1e24be271e373e1a4dcfb4db15e4a81bc1fad7fe
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-03-14 22:29:42 +0700

    register ....

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.DTO.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller
@@ -24,4 +27,17 @@ public class HomePageController {
         model.addAttribute("products", prs);
         return "client/homePage/show";
     }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+
+        return "client/auth/register";
+    }
+
 }
