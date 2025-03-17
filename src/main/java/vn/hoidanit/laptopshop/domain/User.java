954caec88ba8244d52c785e5c3556866f878a931
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-03-17 22:37:39 +0700

    end video 131

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
@@ -42,6 +43,10 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
+
+    //
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
 
     @Override
     public String toString() {
@@ -119,5 +124,13 @@ public class User {
 
     public void setAvatar(String avatar) {
         this.avatar = avatar;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
     }
 }
